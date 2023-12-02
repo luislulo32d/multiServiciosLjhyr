776e6c9240526c00b7f7537860e8a4d5ead640f2
Kai Luis Díaz <luislulo32d@gmail.com>
2023-12-02 18:43:02 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+	require_once 'includes/header.php';
+?>
+<section class="full-width text-center" style="padding: 40px 0;">
+			<h3 class="text-center tittles">Bienvenido al Sistema de Gestión de Inventario Ljhyr</h3>
+			<!-- Tiles -->
+			<article class="full-width tile">
+				<div class="tile-text">
+					<span class="text-condensedLight">
+						<small>Inventario</small>
+					</span>
+				</div>
+				<i class="zmdi zmdi-store tile-icon"></i>
+			</article>
+			<article class="full-width tile">
+				<div class="tile-text">
+					<span class="text-condensedLight">
+						<small>Categorias</small>
+					</span>
+				</div>
+				<i class="zmdi zmdi-label tile-icon"></i>
+			</article>
+			<article class="full-width tile">
+				<div class="tile-text">
+					<span class="text-condensedLight">
+						<small>Productos</small>
+					</span>
+				</div>
+				<i class="zmdi zmdi-washing-machine tile-icon"></i>
+			</article>
+			
